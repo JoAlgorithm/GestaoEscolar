6ff2fced73a2e74b2efc9d6e05a05103cc619507
manuelacacio <manuelacacio40@gmail.com>
2019-07-21 14:45:59 +0200

    ultima alteracao

--- src/app/estudante/listagem/listagem.component.ts
+++ src/app/estudante/listagem/listagem.component.ts
@@ -8,6 +8,7 @@ import { Inject} from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { Turma } from '../../classes/turma';
 import {FormBuilder, FormGroup, Validators,FormControl} from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 
 
@@ -22,27 +23,56 @@ export class ListagemComponent implements OnInit {
   //private alunos:any []=[];
   estudantes: Estudante[];
   isLinear = true;
+  
   //public paginaAtual = 1;
  // filter: any;
   //estudantesfilter: Estudante[];
   //firstFormGroup: FormGroup;
+  generos = [
+    {value: 'Feminino', viewValue: 'Feminino'},
+    {value: 'Masculino', viewValue: 'Masculino'}
+  ];
 
+  documentos_identificacao = [
+    {value: 'BI', viewValue: 'Bilhete de identidade'},
+    {value: 'Cedula', viewValue: 'Cedula'},
+    {value: 'Passaporte', viewValue: 'Passaporte'},
+    {value: 'Certidao de nascimento', viewValue: 'Certidao de nascimento'},
+    {value: 'DIRE', viewValue: 'DIRE'},
+    {value: 'Outros', viewValue: 'Outros'},
+  ]
+
+  nacionalidades = [
+    {value: 'Mocambicana', viewValue: 'Mocambicana'},
+    {value: 'Sul Africana', viewValue: 'Mocambicana'}
+  ]
+
+  provincias = [
+    {value: 'Maputo', viewValue: 'Maputo'},
+    {value: 'Gaza', viewValue: 'Gaza'},
+    {value: 'Inhambane', viewValue: 'Inhambane'},
+    {value: 'Sofala', viewValue: 'Sofala'},
+    {value: 'Tete', viewValue: 'Tete'},
+    {value: 'Quelimane', viewValue: 'Quelimane'},
+    {value: 'Nampula', viewValue: 'Nampula'},
+    {value: 'Cabo Delgado', viewValue: 'Cabo Delgado'},
+    {value: 'Niassa', viewValue: 'Niassa'},
+    {value: 'Zambezia', viewValue: 'Zambezia'}
+  ]
+  data_nascimento: Date;
   dataSourse: MatTableDataSource<Estudante>;
   displayedColumns = ['nome', 'turma', 'nivel', 'regime', 'contacto', 'Detalhe','Editar'];
   @ViewChild(MatPaginator) paginator: MatPaginator;
 @ViewChild(MatSort) sort: MatSort;
 
 firstFormGroup: FormGroup;
-
+estudante: Estudante;
  
-  constructor( private _formBuilder: FormBuilder, public dialog: MatDialog, private estudanteSevice: EstudanteService) { 
+  constructor(  private _formBuilder: FormBuilder, public dialog: MatDialog, private estudanteSevice: EstudanteService) { 
     
   }
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      nome: ['', Validators.required],
-      
-    });
+    
     this.estudanteSevice.getEstudantes().subscribe(data => {
       this.estudantes = data.map(e => {
         return {
@@ -86,11 +116,13 @@ firstFormGroup: FormGroup;
  });
  
   }
-  editar(aluno){
+  editar(aluno: Estudante){
+
   const dialogRef = this.dialog.open(DialogEditar, {
     width: '1000px',
-    data: {nome: aluno.nome, genero: aluno.genero, documento: aluno.documento_identificacao,nacionalidade: aluno.nacionalidade,nr_documento: aluno.nr_documento,
-      local_emissao: aluno.local_emissao,nome_encarregado: aluno.encarregado.nome,datanascimento: aluno.data_nascimento}
+    data:{estudante: aluno, data_nascimento:new Date(aluno.data_nascimento),  generos: this.generos, provincias: this.provincias, documentos_identificacao: this.documentos_identificacao}
+    //data: {estudante_nome: aluno.nome, genero: aluno.genero, documento: aluno.documento_identificacao,nacionalidade: aluno.nacionalidade,nr_documento: aluno.nr_documento,
+    // local_emissao: aluno.local_emissao,nome_encarregado: aluno.encarregado.nome,datanascimento: aluno.data_nascimento}
   });
 
  dialogRef.afterClosed().subscribe(result => {
