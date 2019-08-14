d1351921d2584f86e60830b9c371a8a6d0905aae
JonesAlgorithm <jones.lipeque@algorithm.co.mz>
2019-08-14 20:32:56 +0200

    Resolvendo conflitos de Mesclagem no meu PC

--- src/app/dashboard/dashboard.routing.ts
+++ src/app/dashboard/dashboard.routing.ts
@@ -4,13 +4,9 @@ import { DashboardComponent } from './dashboard.component';
 
 export const DashboardRoutes: Routes = [{
   path: 'dashboard',
-<<<<<<< HEAD
-  component: DashboardComponent,
+  component: DashboardComponent/*,
  children: [
    {path: 'lista_estudante', component: ListaEstudanteComponent}
- ]
-=======
-  component: DashboardComponent
->>>>>>> 16021d572ce9b078f78f169aa7dcd8febb5af0fe
+ ]*/
 }];
 
